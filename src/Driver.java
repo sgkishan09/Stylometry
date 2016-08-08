27b99ab07edd13ff6b7e690ef2c91a360bfadab5
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-08 13:54:27 +0530

    Added Author class

--- src/Driver.java
+++ src/Driver.java
@@ -16,19 +16,11 @@ public class Driver {
 	public static void main(String[] args) {
 
 		try {
-
-			Book book1 = new Book("War And Peace", "Leo Tolstoy", "WarAndPeace.txt");
-			System.out.println(book1);
-			
-			Metric sm = new SentenceMetric(book1);
-			Metric pm = new PunctuationMetric(book1);
-
-			System.out.println(sm + "\n SENTENCE Second Metric: " + sm);
-			System.out.println(sm.compare(sm));
-			
-			System.out.println(pm.compare(pm));
-			System.out.println(pm + "\n Second Metric: " + pm);
-			
+			Author author = new Author("Leo Tolstoy", WordFrequencyMetric.class);
+			Book book1 = new Book("A Child's Dream of a Star", "Charles Dickens", "ChildsDream.txt");
+			Book book2 = new Book("War And Peace", "Leo Tolstoy", "WarAndPeace.txt");
+			author.addBook(book2);
+			author.printAll();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
