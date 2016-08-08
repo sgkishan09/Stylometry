1d0b43888459bb2c51df67b548d1f6fdad30e9b2
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-08 09:33:07 +0530

    Converted to a Maven project

--- src/Book.java
+++ src/Book.java
@@ -5,17 +5,21 @@ public class Book {
 	String author;
 	String name;
 	ArrayList<String> lines;
-	
+
 	public Book() {
 		author = "";
 		name = "";
 		lines = new ArrayList<>();
 	}
 
-	public Book(String author, String name, String path) throws Exception {
+	public Book(String name, String author, String path) throws Exception {
 		this.author = author;
 		this.name = name;
 		this.lines = Util.readFile(path);
 	}
-	
+
+	public String toString() {
+		String out = author + "\t" + name + "\t" + lines.size();
+		return out;
+	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,7 @@ public class Driver {
 	public static void main(String[] args) {
 		try {
 			Book book1 = new Book("War And Peace", "Leo Tolstoy", "WarAndPeace.txt");
+			System.out.println(book1);
 		} catch (Exception e) {
 
 		}
