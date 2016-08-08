93a73694b87f915927dac43c5282e3d358333b6d
Aditya Anand <adianand@paypal.com>
2016-08-08 13:47:04 +0530

    Added Sentence and Punctuation Metric

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,16 @@ public class Driver {
 		try {
 			Book book1 = new Book("War And Peace", "Leo Tolstoy", "WarAndPeace.txt");
 			System.out.println(book1);
+			
+			Metric sm = new SentenceMetric(book1);
+			Metric pm = new PunctuationMetric(book1);
+
+			System.out.println(sm + "\n SENTENCE Second Metric: " + sm);
+			System.out.println(sm.compare(sm));
+			
+			System.out.println(pm.compare(pm));
+			System.out.println(pm + "\n Second Metric: " + pm);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
