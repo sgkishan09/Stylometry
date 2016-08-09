4133e6220aef641811d18b2f795902c552ce0a64
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-09 10:28:56 +0530

    Text Generator for given Author model

--- src/Driver.java
+++ src/Driver.java
@@ -11,42 +11,17 @@ import opennlp.tools.ngram.NGramGenerator;
 
 public class Driver {
 
-	public static HashMap<String, Integer> addToMap(List<String> list) {
-		HashMap<String, Integer> countMap = new HashMap<>();
-		for (String word : list) {
-			if (countMap.containsKey(word))
-				countMap.put(word, countMap.get(word) + 1);
-			else
-				countMap.put(word, 1);
-		}
-		return countMap;
-	}
-
-	public static HashMap<String, Integer> frequency(List<String> bigram, String currentWord) {
-		List<String> startingWords = bigram.stream().filter(p -> p.split(" ")[0].equals(currentWord))
-				.map(p -> p.split(" ")[1]).collect(Collectors.toList());
-		return addToMap(startingWords);
-	}
-
-	public static HashMap<String, Integer> startSentence(List<String> sentences) {
-		return addToMap(sentences.stream().map(p -> p.split(" ")[0]).collect(Collectors.toList()));
-	}
 	public static void main(String[] args) {
 		try {
-			/*
-			 * Author author = new Author("Shakespeare", SentenceMetric.class);
-			 * author.addBook(new Book("Othello", "Shakespeare",
-			 * "Othello.txt")); author.addBook(new Book("JuliusCaesar",
-			 * "Shakespeare", "Julius Caesar.txt")); author.addBook(new
-			 * Book("Macbeth", "Shakespeare", "Macbeth.txt"));
-			 */
-			Book book = new Book("War and Peace", "Tolstoy", "Othello.txt");
-			SentenceMetric metric=new SentenceMetric(book);
-			System.out.println(metric.getWordsPerSentence());
-			NGramGenerator generator = new NGramGenerator();
-			List<String> words = NGramGenerator.generate(book.words, 1, " ");
-			List<String> bigrams = NGramGenerator.generate(book.words, 2, " ");
-			System.out.println(startSentence(book.sentences));
+			Author author = new Author("Shakespeare", SentenceMetric.class);
+			author.addBook(new Book("Othello", "Shakespeare", "Othello.txt"));
+			author.addBook(new Book("JuliusCaesar", "Shakespeare", "Julius Caesar.txt"));
+			author.addBook(new Book("Macbeth", "Shakespeare", "Macbeth.txt"));
+			author.setupCorpus();
+			for (int i = 0; i < 10; i++) {
+				System.out.println(author.generateSentence());
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/entities/Author.java
+++ src/entities/Author.java
@@ -1,21 +1,82 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
+import java.util.stream.Collectors;
+import metrics.*;
 import metrics.Metric;
+import opennlp.tools.ngram.NGramGenerator;
+import opennlp.tools.ngram.NGramModel;
 
 public class Author {
 	String name;
 	ArrayList<Book> books;
-	public HashMap<String, Integer> wordCorpus = new HashMap<String, Integer>();
-	public HashMap<String, Integer> letterCorpus = new HashMap<String, Integer>();
+	public List<String> sentenceCorpus = new ArrayList<String>();
+	public List<String> wordCorpus = new ArrayList<String>();
+	public List<String> bigrams;
+	public HashMap<String, Integer> startWordMap;
+	public double avgWordsPerSentence;
 	Class metricType;
+
 	public Author() {
 		books = new ArrayList<>();
+	}
+
+	public void setupCorpus() {
+		int totalSentenceCount = 0;
+		for (Book book : books) {
+			sentenceCorpus.addAll(book.sentences);
+			avgWordsPerSentence += (new SentenceMetric(book).getWordsPerSentence() * book.sentences.size());
+			totalSentenceCount += book.sentences.size();
+			wordCorpus.addAll(book.words);
+		}
+		avgWordsPerSentence /= totalSentenceCount;
+		bigrams = NGramGenerator.generate(wordCorpus, 5, " ");
+		bigrams = bigrams.stream().map(p -> p.toLowerCase()).collect(Collectors.toList());
+		startWordMap = startSentence(sentenceCorpus);
+	}
+
+	public String generateSentence() {
+		int count = (int) avgWordsPerSentence + ((int) Math.random() * 100 % 10);
+		String out = "";
+		String current = nextWord(startWordMap);
+		out = current;
+		for (int i = 0; i < count; i++) {
+			String temp = nextWord(frequency(current));
+			while (temp == null) {
+				temp = nextWord(addToMap(wordCorpus));
+			}
+			current = temp;
+			out += " " + current;
+		}
+		return out;
+	}
+
+	public HashMap<String, Integer> addToMap(List<String> list) {
+		HashMap<String, Integer> countMap = new HashMap<>();
+		for (String word : list) {
+			if (countMap.containsKey(word))
+				countMap.put(word, countMap.get(word) + 1);
+			else
+				countMap.put(word, 1);
+		}
+		return countMap;
+	}
+
+	public HashMap<String, Integer> frequency(String currentWord) {
+		List<String> nextWordList = new ArrayList<String>();
+		nextWordList = bigrams.stream().map(p -> p.split(" ")[p.split(" ").length - 1]).collect(Collectors.toList());
+		return addToMap(nextWordList);
+	}
+
+	public HashMap<String, Integer> startSentence(List<String> sentences) {
+		return addToMap(
+				sentences.stream().map(p -> p.split(" ")[0]).map(p -> p.toLowerCase()).collect(Collectors.toList()));
 	}
 
 	public Author(String name, Class metricType) {
@@ -59,49 +120,22 @@ public class Author {
 		}
 	}
 
-	public void generateCorpusList() {
-		for (Book book : books) {
-			ArrayList<String> words = book.words;
-			for (String word : words) {
-				if (wordCorpus.containsKey(word))
-					wordCorpus.put(word, wordCorpus.get(word) + 1);
-				else
-					wordCorpus.put(word, new Integer(1));
-			}
-		}
-
-	}
-
-	public void generateLetterList() {
-		for (char i = 'a'; i < 'z'; i++) {
-			letterCorpus.put(String.valueOf(i), 0);
-		}
-
-		for (Book book : books) {
-			for (Map.Entry<String, Integer> entry : letterCorpus.entrySet()) {
-				letterCorpus.put(entry.getKey(), entry.getValue()
-						+ (book.content.length() - book.content.replaceAll(entry.getKey(), "").length()));
-			}
-		}
-	}
-	
-	public String nextWord(Map<String, Integer> freqMap) {		
+	public String nextWord(Map<String, Integer> freqMap) {
 		Map<String, Double> probMap = new LinkedHashMap<String, Double>();
 		int totalSum = 0;
 		double sumProb = 0.0;
-		for(Integer i : freqMap.values()) {
+		for (Integer i : freqMap.values()) {
 			totalSum += i;
 		}
-		
-		for(Map.Entry<String, Integer> entry : freqMap.entrySet()) {
-			probMap.put(entry.getKey(), sumProb + (double)entry.getValue() / totalSum);
-			sumProb += (double)entry.getValue() / totalSum;
+
+		for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+			probMap.put(entry.getKey(), sumProb + (double) entry.getValue() / totalSum);
+			sumProb += (double) entry.getValue() / totalSum;
 		}
-		
-		
+
 		double rnd = Math.random();
-		for(Map.Entry<String, Double> entry : probMap.entrySet()) {
-			if(entry.getValue() >= rnd)
+		for (Map.Entry<String, Double> entry : probMap.entrySet()) {
+			if (entry.getValue() >= rnd)
 				return entry.getKey();
 		}
 		return null;
